3344876ad53bfaafad51862d25eaaded32884837
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 15:00:36 -0600

    feat(seeding): Create a seeder factory for the User following the documentation available in the link below https://www.npmjs.com/package/typeorm-extension

---
+++ database/factories/user.factory.ts
@@ -0,0 +1,11 @@
+import { setSeederFactory } from 'typeorm-extension';
+import { UserEntity } from '../../apps/users/src/infrastructure/domain/user.entity';
+
+export default setSeederFactory(UserEntity, (faker) => {
+  const user = new UserEntity();
+  user.name = faker.name.firstName('male');
+  user.email = faker.internet.email(user.name);
+  user.bio = faker.commerce.productDescription();
+
+  return user;
+})
