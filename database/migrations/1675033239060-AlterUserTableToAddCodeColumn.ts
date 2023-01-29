19f60b18ef2d123830cc11d4616022b495eae7b3
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 17:04:37 -0600

    feat(migration): Generate the migration to create the new "code" column

---
+++ database/migrations/1675033239060-AlterUserTableToAddCodeColumn.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AlterUserTableToAddCodeColumn1675033239060 implements MigrationInterface {
+    name = 'AlterUserTableToAddCodeColumn1675033239060'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" ADD "code" character varying(6)`);
+        await queryRunner.query(`ALTER TABLE "users" ADD CONSTRAINT "UQ_1f7a2b11e29b1422a2622beab36" UNIQUE ("code")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "users" DROP CONSTRAINT "UQ_1f7a2b11e29b1422a2622beab36"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "code"`);
+    }
+
+}
