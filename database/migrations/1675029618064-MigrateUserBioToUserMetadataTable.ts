3585dd8096e8c087ad9889976f27d5857c067918
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 16:09:48 -0600

    feat(migration): Generate migration to copy the user bio field to the new metadata table

---
+++ database/migrations/1675029618064-MigrateUserBioToUserMetadataTable.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class MigrateUserBioToUserMetadataTable1675029618064 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`
+        INSERT INTO users_metadata (bio, user_id)
+        SELECT users.bio, users.id FROM users
+      `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+}
