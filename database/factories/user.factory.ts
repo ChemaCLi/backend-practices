1c840ae8ae1edd1640ee668e5e3a63668905dd69
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 21:11:01 -0600

    feat(seeder): Insert user metadata with the user factory

--- database/factories/user.factory.ts
+++ database/factories/user.factory.ts
@@ -1,10 +1,19 @@
 import { setSeederFactory } from 'typeorm-extension';
 import { UserEntity } from '../../apps/users/src/infrastructure/domain/user.entity';
+import {UsersMetadataEntity} from "../../apps/users/src/infrastructure/domain/users-metadata.entity";
 
 export default setSeederFactory(UserEntity, (faker) => {
   const user = new UserEntity();
+  const userMetadata = new UsersMetadataEntity()
+  Object.assign(userMetadata, {
+    bio: faker.lorem.text(),
+    image: faker.image.imageUrl(),
+    birthday: faker.date.birthdate(),
+  })
+
   user.name = faker.name.firstName('male');
   user.email = faker.internet.email(user.name);
+  user.userMetadata = userMetadata;
 
   return user;
 });
