5d0b5a1840649d782430844b88be25b3778d8a0a
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 23:07:19 -0600

    feat(pagination): Implement pagination in the users list controller

--- apps/users/src/infrastructure/controllers/users.controller.ts
+++ apps/users/src/infrastructure/controllers/users.controller.ts
@@ -4,22 +4,37 @@ import {
   Controller,
   Get,
   Post,
+  Query,
 } from '@nestjs/common';
 import { CreateUserCommand } from '../../application/create-user/create-user.command';
 import { CreateUserService } from '../../application/create-user/create-user.service';
 import { ListUsersService } from '../../application/list-users/list-users.service';
 import { IUserSchema } from '../../domain/user';
+import { Paginated } from '../../domain/paginated';
+import { ListUsersCommand } from '../../application/list-users/list-users.command';
+import { CountUsersService } from '../../application/list-users/count-users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly createUserService: CreateUserService,
     private readonly listUsersService: ListUsersService,
+    private readonly countUsersService: CountUsersService,
   ) {}
 
   @Get('/')
-  async list(): Promise<IUserSchema[]> {
-    return this.listUsersService.process();
+  async list(
+    @Query()
+    { page, pageSize }: ListUsersCommand,
+  ): Promise<Paginated<IUserSchema>> {
+    const users = await this.listUsersService.process({ page, pageSize });
+    const totalUsers = await this.countUsersService.process();
+
+    return new Paginated<IUserSchema>(users, {
+      page,
+      pageSize,
+      totalRecords: totalUsers,
+    });
   }
 
   @Post()
