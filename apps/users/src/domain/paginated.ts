17f00e34f690a896088ca5fa91669dd23cbce695
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 22:35:38 -0600

    feat(pagination): Add a decorator to return pagination info in the ListUsers endpoint result

---
+++ apps/users/src/domain/paginated.ts
@@ -0,0 +1,35 @@
+import { IsArray } from 'class-validator';
+
+export interface IPaginationInfo {
+  page: number;
+  pageSize: number;
+  totalPages: number;
+  totalRecords: number;
+  nextPage: boolean;
+  previousPage: boolean;
+}
+
+export class Paginated<T> {
+  @IsArray()
+  readonly entries: T[];
+  readonly paginationInfo: IPaginationInfo;
+
+  constructor(
+    data: T[],
+    pagination: {
+      page: number;
+      pageSize: number;
+      totalRecords: number;
+    },
+  ) {
+    this.entries = data;
+    const totalPages = Math.ceil(pagination.totalRecords / pagination.pageSize);
+
+    this.paginationInfo = {
+      ...pagination,
+      totalPages,
+      previousPage: pagination.page > 1,
+      nextPage: pagination.page < totalPages,
+    };
+  }
+}
