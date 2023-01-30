0f4d5bdfc742b2e48f5b7998aa8f48746f830643
Jose Maria CLi <chema9703@gmail.com>
2023-01-29 20:19:31 -0600

    feat(UpdateCreateUserApplicationService): Generate a random code for new users while being created

--- apps/users/src/application/create-user/create-user.service.ts
+++ apps/users/src/application/create-user/create-user.service.ts
@@ -17,11 +17,23 @@ export class CreateUserService
     const user = new User({
       name,
       email,
-      userMetadata: { bio }
+      code: generateRandomCode(6),
+      userMetadata: { bio },
     });
 
     await this.userRepository.persist(user);
-
     return user.entityRoot();
   }
 }
+
+const generateRandomCode = (maxLength) => {
+  const possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+  let code = "";
+  for (let i = 0; i < maxLength; i++) {
+    code += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+
+  return code;
+}
+
