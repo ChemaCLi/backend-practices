b13e9cd18197265f9a84495803f86b634ad665b4
Jose Maria CLi <chema9703@gmail.com>
2023-01-30 00:37:42 -0600

    feat(sign-image-url): Sign the user images before returning the response
     Using dependency injection: Pass the AssetSigner provider as a collaborator of the user preSignImageUrl

--- apps/users/src/application/get-user/get-user.service.ts
+++ apps/users/src/application/get-user/get-user.service.ts
@@ -2,17 +2,20 @@ import { IApplicationService } from '@app/commons/application/contracts/applicat
 import { Inject, Injectable } from '@nestjs/common';
 import { IUserSchema } from '../../domain/user';
 import { UserRepository } from '../../infrastructure/repositories/user.repository';
-import { USER_REPOSITORY } from '../constants/injection-tokens';
+import { USER_REPOSITORY, ASSET_SIGNER } from '../constants/injection-tokens';
 import { GetUserCommand } from './get-user.command';
+import { IAssetSigner } from '../../domain/contracts/asset-signer';
 
 @Injectable()
 export class GetUserService implements IApplicationService<GetUserCommand> {
   constructor(
     @Inject(USER_REPOSITORY) private userRepository: UserRepository,
+    @Inject(ASSET_SIGNER) private assetSigner: IAssetSigner,
   ) {}
 
   async process(command: GetUserCommand): Promise<IUserSchema> {
     const user = await this.userRepository.getById(command.id);
+    await user.preSignImageUrl(this.assetSigner);
 
     return user.entityRoot();
   }
--- apps/users/src/application/list-users/list-users.service.ts
+++ apps/users/src/application/list-users/list-users.service.ts
@@ -2,18 +2,21 @@ import { IApplicationService } from '@app/commons/application/contracts/applicat
 import { Inject, Injectable } from '@nestjs/common';
 import { IUserSchema } from '../../domain/user';
 import { UserRepository } from '../../infrastructure/repositories/user.repository';
-import { USER_REPOSITORY } from '../constants/injection-tokens';
+import { ASSET_SIGNER, USER_REPOSITORY } from '../constants/injection-tokens';
 import { ListUsersCommand } from './list-users.command';
+import { IAssetSigner } from '../../domain/contracts/asset-signer';
 
 @Injectable()
 export class ListUsersService implements IApplicationService<ListUsersCommand> {
   constructor(
     @Inject(USER_REPOSITORY) private userRepository: UserRepository,
+    @Inject(ASSET_SIGNER) private assetSigner: IAssetSigner,
   ) {}
 
   async process({ page, pageSize }: ListUsersCommand): Promise<IUserSchema[]> {
-    return (await this.userRepository.list({ page, pageSize })).map((user) =>
-      user.entityRoot(),
-    );
+    return (await this.userRepository.list({ page, pageSize })).map((user) => {
+      user.preSignImageUrl(this.assetSigner);
+      return user.entityRoot();
+    });
   }
 }
